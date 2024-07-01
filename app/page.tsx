144c57af4cdd491526952731711fb7c111aee817
austinmargarone <austin@fcvt.dev>
2024-07-01 17:55:30 -0400

    Connect DB

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
 
 export default function Home() {
+  console.log(db);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
