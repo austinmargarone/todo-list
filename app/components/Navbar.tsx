0d3752d5457f7f307d20662e02cf21e4a00b7b24
austinmargarone <austin@fcvt.dev>
2024-07-02 08:46:05 -0400

    feat: Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="h-[3.5rem] bg-black border-b flex justify-start items-center pl-[2.5rem]">
+      <span>To Do List</span>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "To-Do List",
   description: "A simple to-do list app to keep track of your tasks.",
+  keywords: ["to-do", "tasks", "list"],
 };
 
 export default function RootLayout({
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {" "}
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
