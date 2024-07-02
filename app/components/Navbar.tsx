06f505da358f6b803779ba80598cf1e6ff6b60e8
austinmargarone <austin@fcvt.dev>
2024-07-02 13:41:14 -0400

    feat: Navbar button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="h-[3.5rem] bg-black border-b flex justify-start items-center pl-[2.5rem]">
-      <span>To Do List</span>
+    <div className="h-[3.5rem] bg-black border-b flex justify-between items-center px-[2.5rem]">
+      <div>To Do App</div>
+      <div>
+        <Link
+          href={"https://github.com/austinmargarone/todo-list"}
+          target="_blank"
+        >
+          <button className="bg-blue-500 px-3 py-1.5 rounded-sm shadow-lg">
+            Github Repo
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
