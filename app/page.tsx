bf96d3a08bd87d9302fe7a5e4e441ae4a2837d32
austinmargarone <austin@fcvt.dev>
2024-07-03 11:12:08 -0400

    feat: Create a completed task section

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import TodoItem from "./components/TodoItem";
 
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
     async function fetchTodos() {
@@ -15,7 +16,8 @@ export default function Home() {
         (a: Todo, b: Todo) =>
           new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
       );
-      setTodos(sortedTodos);
+      setTodos(sortedTodos.filter((todo: Todo) => !todo.completed));
+      setCompletedTodos(sortedTodos.filter((todo: Todo) => todo.completed));
     }
     fetchTodos();
   }, []);
@@ -31,7 +33,7 @@ export default function Home() {
         completed: false,
       });
       const newTodo = res.data;
-      setTodos([newTodo, ...todos]);
+      setTodos((prevTodos) => [newTodo, ...prevTodos]);
     } catch (error) {
       console.error("Error posting todo", error);
     }
@@ -40,11 +42,20 @@ export default function Home() {
   async function handleToggleCompleted(id: number, completed: boolean) {
     try {
       const res = await axios.patch("/api/todo", { id, completed: !completed });
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, completed: !completed } : todo
-        )
+      const updatedTodo = res.data;
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+      setCompletedTodos((prevCompletedTodos) =>
+        prevCompletedTodos.filter((todo) => todo.id !== id)
       );
+
+      if (updatedTodo.completed) {
+        setCompletedTodos((prevCompletedTodos) => [
+          updatedTodo,
+          ...prevCompletedTodos,
+        ]);
+      } else {
+        setTodos((prevTodos) => [updatedTodo, ...prevTodos]);
+      }
     } catch (error) {
       console.error("Error updating todo", error);
     }
@@ -53,7 +64,10 @@ export default function Home() {
   async function handleDeleteTodo(id: number) {
     try {
       await axios.delete("/api/todo", { data: { id } });
-      setTodos(todos.filter((todo) => todo.id !== id));
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+      setCompletedTodos((prevCompletedTodos) =>
+        prevCompletedTodos.filter((todo) => todo.id !== id)
+      );
     } catch (error) {
       console.error("Error deleting todo", error);
     }
@@ -67,9 +81,19 @@ export default function Home() {
     try {
       const res = await axios.patch("/api/todo", { id, title, description });
       const updatedTodo = res.data;
-      setTodos(
-        todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
-      );
+      if (updatedTodo.completed) {
+        setCompletedTodos((prevCompletedTodos) =>
+          prevCompletedTodos.map((todo) =>
+            todo.id === updatedTodo.id ? updatedTodo : todo
+          )
+        );
+      } else {
+        setTodos((prevTodos) =>
+          prevTodos.map((todo) =>
+            todo.id === updatedTodo.id ? updatedTodo : todo
+          )
+        );
+      }
     } catch (error) {
       console.error("Error editing todo", error);
     }
@@ -78,7 +102,7 @@ export default function Home() {
   return (
     <main className="py-[2.5rem] flex flex-col items-center gap-[2.5rem] justify-center">
       <TodoForm onCreateTodo={handlePostTodo} />
-      <ul className="gap-[2rem] flex flex-col border-t w-[280px] sm:w-[325px] md:w-[350px] lg:w-[380px]">
+      <section className="gap-[2rem] flex flex-col border-t w-[280px] sm:w-[325px] md:w-[350px] lg:w-[380px]">
         <h2 className="flex justify-center mt-[2.5rem]">My Tasks</h2>
         {todos.map((todo) => (
           <TodoItem
@@ -89,7 +113,19 @@ export default function Home() {
             onEditTodo={handleEditTodo}
           />
         ))}
-      </ul>
+      </section>
+      <section className="gap-[2rem] flex flex-col border-t w-[280px] sm:w-[325px] md:w-[350px] lg:w-[380px]">
+        <h2 className="flex justify-center mt-[2.5rem]">Completed Tasks</h2>
+        {completedTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggleCompleted={handleToggleCompleted}
+            onDeleteTodo={handleDeleteTodo}
+            onEditTodo={handleEditTodo}
+          />
+        ))}
+      </section>
     </main>
   );
 }
