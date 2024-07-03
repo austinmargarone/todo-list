aaa147f71c0e9eb543330c5555a2f8b6519ba0ec
austinmargarone <austin@fcvt.dev>
2024-07-03 15:14:29 -0400

    feat: Meta image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,20 @@ import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://prisma-to-do-list.vercel.app/"),
   title: "To-Do List",
   description: "A simple to-do list app to keep track of your tasks.",
   keywords: ["to-do", "tasks", "list"],
+  openGraph: {
+    images: [
+      {
+        url: `https://res.cloudinary.com/dfah2ntps/image/upload/v1720033700/todo_blzt3b.png`,
+        width: 1200,
+        height: 630,
+        alt: "Full Stack Developer",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
