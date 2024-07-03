2f8d4416bb46faf52b34c3373acffc244b67cd66
austinmargarone <austin@fcvt.dev>
2024-07-03 11:39:20 -0400

    feat: Navbar button style

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,14 +3,16 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="h-[4.25rem] bg-black border-b flex justify-between items-center px-[2.5rem]">
-      <div className="navbar-title">To Do App</div>
+    <div className="h-[4.25rem] bg-black border-b flex justify-between items-center px-6 md:px-10">
+      <div className="navbar-title text-white text-2xl font-bold">
+        To Do App
+      </div>
       <div>
         <Link
           href={"https://github.com/austinmargarone/todo-list"}
           target="_blank"
         >
-          <button className="bg-blue-500 px-3 py-1.5 rounded-sm shadow-lg">
+          <button className="bg-blue-500 hover:bg-blue-600 transition-colors duration-300 px-4 py-2 rounded-md shadow-md text-white">
             Github Repo
           </button>
         </Link>
